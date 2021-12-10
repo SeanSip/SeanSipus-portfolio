bf4841aff2b0ddcf05f47b0036beee0bc6923365
SeanSip <sean.sipus@gmail.com>
2021-12-10 07:18:53 -0500

    w.i.p. added styling to about and contact sections

---
+++ index.js
@@ -0,0 +1,19 @@
+// const hamburgerMenu = document.querySelector('.hamburgerMenu');
+// const navMenu = document.querySelector('.navMenu');
+
+// hamburgerMenu.addEventListener("click", () => {
+//   hamburgerMenu.classList.toggle("active");
+//   navMenu.classList.toggle("active");
+  
+// })
+
+// document.querySelector(".hamburgerMenu").onclick = function (event) {
+//     const hamburgerMenu = document.querySelector('.hamburgerMenu');
+//     const navMenu = document.querySelector('.navMenu');
+
+//     hamburgerMenu.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+
+
+//     e.preventDefault();
+// }
